952d486052bf23ec879407b95cfb4d9b8919100e
jakelorenzesabal <jakelorenzecan.com>
2024-11-19 22:56:38 +0800

    adding quantity in order

--- src/app/_models/order.ts
+++ src/app/_models/order.ts
@@ -3,6 +3,7 @@ import { Account} from '@app/_models';
 export interface Order {
     id: string;
     totalAmount: string;
+    quantity: string;
     shippingAddress: string;
     orderStatus: string;
     createdAt: string;
