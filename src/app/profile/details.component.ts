eec0af3c76e29b3158936acbdd321a94a44232c5
jakelorenzesabal <jakelorenzecan.com>
2024-11-24 00:07:57 +0800

    adding activitylog all

--- src/app/profile/details.component.ts
+++ src/app/profile/details.component.ts
@@ -10,12 +10,18 @@ export class DetailsComponent implements OnInit {
     account = this.accountService.accountValue;
     activityLogs: ActivityLog[] = [];
     filteredLogs: ActivityLog[] = [];
+    allActivityLogs: ActivityLog[] = [];
+    filteredAllLogs: ActivityLog[] = [];
     branch: Branch | null = null;
     showActivityLogs = false;
+    showAllActivityLogs = false;
     showBranchInfo: boolean = false;
     searchTerm: string = '';
     startDate: string = '';
     endDate: string = '';
+    adminSearchTerm: string = '';
+    adminStartDate: string = '';
+    adminEndDate: string = '';
 
     constructor(
         private accountService: AccountService,
@@ -57,6 +63,32 @@ export class DetailsComponent implements OnInit {
         });
     }
 
+    getAllActivityLogs(): void {
+        this.accountService.getAllActivityLogs().subscribe({
+            next: (logs: ActivityLog[]) => {
+                this.allActivityLogs = logs;
+                this.filteredAllLogs = logs;
+            },
+            error: (error) => {
+                console.error('Error fetching activity logs:', error);
+            }
+        });
+    }
+
+    handleAdminSearch(): void {
+        const startDate = this.adminStartDate ? new Date(this.adminStartDate).getTime() : null;
+        const endDate = this.adminEndDate ? new Date(this.adminEndDate).getTime() : null;
+
+        this.filteredAllLogs = this.allActivityLogs.filter((log: ActivityLog) => {
+            const logTimestamp = new Date(log.timestamp).getTime();
+            const matchesSearchTerm = log.actionType.toLowerCase().includes(this.adminSearchTerm.toLowerCase());
+            const matchesStartDate = !startDate || logTimestamp >= startDate;
+            const matchesEndDate = !endDate || logTimestamp <= endDate;
+
+            return matchesSearchTerm && matchesStartDate && matchesEndDate;
+        });
+    }
+
     getBranchById(branchId: string): void {
         this.branchService.getBranchById(branchId).subscribe(
             (branch) => {
@@ -66,6 +98,13 @@ export class DetailsComponent implements OnInit {
                 console.error('Error fetching branch:', error);
             }
         );
+    }
+
+    toggleAllActivityLogs(): void {
+        if (!this.showAllActivityLogs) {
+            this.getAllActivityLogs();  // Fetch the logs only when toggled
+        }
+        this.showAllActivityLogs = !this.showAllActivityLogs;
     }
 
     toggleActivityLogs(): void {
@@ -78,5 +117,9 @@ export class DetailsComponent implements OnInit {
 
     isManager(): boolean {
         return this.account?.role === 'Staff';
+    }
+
+    isAdmin(): boolean {
+        return this.account?.role === 'Admin';
     }
 }
