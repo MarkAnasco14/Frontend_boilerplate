259b2ca5c52bd752a1af11a9667a49985d45dc29
Jake Rojas <jakerojas2211@gmail.com>
2024-12-03 21:31:52 +0800

    updated somthing

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,10 @@
+// export const environment = {
+//     production: true,
+//     apiUrl: 'http://localhost:4000'
+// };
+
+
 export const environment = {
     production: true,
-    apiUrl: 'http://localhost:4000'
+    apiUrl: 'https://myproject.com'
 };
